9dad2a0d9a3b570bf74de2b3c7f559608dee8191
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-04 14:17:13 +0000

    JS fixed

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let length;
-let area;
-document.getElementById('squarearea').addEventListener('click', solvefunction);
-  function solvefunction(){
-length=document.getElementById('variable').value;
-  area=length*length
-  alert(area);
+let length
+let area
+document.getElementById('squarearea').addEventListener('click', solvefunction)
+function solvefunction (){
+  length = document.getElementById('variable').value
+  area = length * length
+  alert(area)
 }
