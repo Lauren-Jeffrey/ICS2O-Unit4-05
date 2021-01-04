8e72389d967e2986159a0fecb66e44c5e0719883
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-04 13:58:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let length;
+let area;
+document.getElementById('squarearea').addEventListener('click', solvefunction);
+  function solvefunction(){
+length=document.getElementById('variable').value;
+  area=length*length
+  alert(area);
+}
